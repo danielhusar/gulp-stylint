c4eb9a41c7183701f91ca43898f34173312db7b5
Daniel Husar <dano.husar@gmail.com>
2015-03-30 15:54:25 +0100

    simplify tests

--- test.js
+++ test.js
@@ -1,35 +1,35 @@
 'use strict';
+
 var assert = require('assert');
 var gutil = require('gulp-util');
 var sinon = require('sinon');
 var stylint = require('./');
 
-it('It should not log if file is valid', function (cb) {
-	var log = sinon.spy();
-	var stream = stylint({}, log);
+var log = sinon.spy();
+var stream;
 
+function file (file) {
+	stream.write(new gutil.File({
+		base: __dirname,
+		path: __dirname + '/fixtures/' + file,
+		contents: new Buffer('')
+	}));
 	stream.on('data', function () {});
+	stream.end();
+}
 
+it('It should not log if file is valid', function (cb) {
+	stream = stylint({}, log);
 	stream.on('end', function () {
 		assert(!log.called);
 		cb();
 	});
 
-	stream.write(new gutil.File({
-		base: __dirname,
-		path: __dirname + '/fixtures/valid.styl',
-		contents: new Buffer('')
-	}));
-
-	stream.end();
+	file('valid.styl');
 });
 
 it('It should log zero units', function (cb) {
-	var log = sinon.spy();
-	var stream = stylint({}, log);
-
-	stream.on('data', function () {});
-
+	stream = stylint({}, log);
 	stream.on('end', function () {
 		var warnings = log.getCall(0).args[0].split('\n');
 
@@ -38,26 +38,17 @@ it('It should log zero units', function (cb) {
 		cb();
 	});
 
-	stream.write(new gutil.File({
-		base: __dirname,
-		path: __dirname + '/fixtures/novalid.styl',
-		contents: new Buffer('')
-	}));
-
-	stream.end();
+	file('novalid.styl');
 });
 
 
 it('It should log zero units and colon warning with custom options', function (cb) {
-	var log = sinon.spy();
-	var stream = stylint({
+	stream = stylint({
 		config: 'fixtures/.configrc'
 	}, log);
 
-	stream.on('data', function () {});
-
 	stream.on('end', function () {
-		var warnings = log.getCall(0).args[0].split('\n');
+		var warnings = log.getCall(1).args[0].split('\n');
 
 		assert.equal(warnings[0].trim(), 'Warning:  unecessary colon found:');
 		assert.equal(warnings[2].trim(), 'Line: 2: margin: 0px');
@@ -66,11 +57,5 @@ it('It should log zero units and colon warning with custom options', function (c
 		cb();
 	});
 
-	stream.write(new gutil.File({
-		base: __dirname,
-		path: __dirname + '/fixtures/novalid.styl',
-		contents: new Buffer('')
-	}));
-
-	stream.end();
+	file('novalid.styl');
 });
